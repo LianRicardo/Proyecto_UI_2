485523431f882da36c3b481f86dc9e71ad34e035
LianRicardo <idv16c.lcamejo@uartesdigitales.edu.mx>
2018-03-05 21:03:14 -0600

    ya esta hecho hasta donde jalil me dijo

--- Proyecto_UI_2/Proyecto_UI_2/Form2.cs
+++ Proyecto_UI_2/Proyecto_UI_2/Form2.cs
@@ -16,5 +16,93 @@ namespace Proyecto_UI_2
         {
             InitializeComponent();
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            celda w = new celda(); ;
+            w.createcell();
+            
+        }
     }
 }
+
+class celda
+{ 
+    public int x;
+    public int y;
+    public PictureBox g;
+    public int n_celdas;
+
+    public celda()
+    {
+
+    }
+
+    ~celda()
+    {
+        x = 0;
+        y = 0;
+        g = null;
+        n_celdas = 0;
+    }
+    public void createcell()
+    {
+        int j = 0;
+        int i = 0;
+        j = 10;
+        for (i = 0; i < j; i++)
+        {
+            celda ncelda = new celda();
+            ncelda.n_celdas++;
+            ncelda.x = ncelda.n_celdas;
+            ncelda.y = ncelda.n_celdas;
+        }
+    }
+};
+
+/*   private void Form2_Load(object sender, EventArgs e)
+       {
+            private celda[] tablero = new celda[] { };
+            int i = 0;
+            int j = 10;
+            for(i=0; i < j; i++)
+            {
+                celda ncelda = new celda();
+                ncelda.n_celdas ++
+                ncelda.x = ncelda.n_celdas += 1;
+                ncelda.y = ncelda.nceldas -= 1;
+            }
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
+
+class celda
+{
+    int x;
+    int y;
+    PictureBox g;
+    int n_celdas;
+
+    celda ()
+    {
+    x=0;
+    y=0;
+    g=null;
+    n_celdas=0;
+    }
+
+    ~celda()
+    {
+    g.close();
+    delete celda
+}*/
